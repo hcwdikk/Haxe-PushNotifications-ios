29a151cd5c39526f78dc7d0a946c1f4bae7a229a
Mateusz Narolewski <mateusz.narolewski@socializer.pl>
2014-08-11 13:37:29 +0200

    Example

---
+++ src/Main.hx
@@ -0,0 +1,57 @@
+package ;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.Lib;
+
+/**
+ * ...
+ * @author Mateusz Narolewski
+ */
+
+class Main extends Sprite 
+{
+	var inited:Bool;
+
+	/* ENTRY POINT */
+	
+	function resize(e) 
+	{
+		if (!inited) init();
+	}
+	
+	function init() 
+	{
+		if (inited) return;
+		inited = true;
+
+		//Logs device's token in console
+		PushNotifications.getDeviceToken();
+	}
+
+	/* SETUP */
+
+	public function new() 
+	{
+		super();	
+		addEventListener(Event.ADDED_TO_STAGE, added);
+	}
+
+	function added(e) 
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, added);
+		stage.addEventListener(Event.RESIZE, resize);
+		#if ios
+		haxe.Timer.delay(init, 100); // iOS 6
+		#else
+		init();
+		#end
+	}
+	
+	public static function main() 
+	{
+		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		Lib.current.addChild(new Main());
+	}
+}
